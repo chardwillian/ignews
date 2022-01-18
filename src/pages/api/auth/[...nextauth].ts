a526f6abae92db1082ac73c9502a89c76523a3dc
chardwillian <69554100+chardwillian@users.noreply.github.com>
2022-01-18 10:13:37 -0300

    Continuando a página posts
    Listando posts em tela, criando navegação do menu usando o componente ActiveLink e validando a assinatura ativa.

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -5,7 +5,6 @@ import GithubProvider from "next-auth/providers/github"
 import { fauna } from '../../../services/fauna';
 
 export default NextAuth({
-  // Configure one or more authentication providers
   providers: [
     GithubProvider({
       clientId: process.env.GITHUB_CLIENT_ID,
@@ -18,6 +17,44 @@ export default NextAuth({
     }),
   ],
   callbacks: {
+    //async session(session) {
+      async session({ session }) {
+    // session: async ({ session }) => {
+     //session.user.email
+      try {
+      const userActiveSubscription = await fauna.query(
+        q.Get(
+          q.Intersection([
+            q.Match(
+              q.Index('subscription_by_user_ref'),
+              q.Select(
+                "ref",
+                q.Get(
+                  q.Match(
+                    q.Index('user_by_email'),
+                    q.Casefold(session.user.email)
+                  )
+                )
+              )
+            ),
+            q.Match(
+              q.Index('subscription_by_status'),
+              "active"
+            )
+          ])
+        )
+      )
+      return {
+        ...session,
+        activeSubscription: userActiveSubscription
+      }
+      } catch {
+        return {
+          ...session,
+          activeSubscription: null,
+        }
+      }
+    },
     async signIn({user, account, profile}) {
       const { email } = user
 
